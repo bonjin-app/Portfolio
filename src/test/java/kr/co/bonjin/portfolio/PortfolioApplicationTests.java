751627fd00881ccb46d45de0f73506ffef7efdce
bonjin-app <bonjin-app@gmail.com>
2021-01-19 08:46:57 +0900

    Initial commit

---
+++ src/test/java/kr/co/bonjin/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
