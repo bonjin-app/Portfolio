820c03ce98b38d5340f726a7b0e2a07ed8623cc6
bonjin-app <bonjin-app@gmail.com>
2021-01-19 14:17:51 +0900

    Reply Column 추가.

--- src/main/java/kr/co/bonjin/portfolio/entity/Reply.java
+++ src/main/java/kr/co/bonjin/portfolio/entity/Reply.java
@@ -18,5 +18,11 @@ public class Reply extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 이름
+    @Column(name = "name")
+    private String name;
 
+    // 내용
+    @Column(name = "content")
+    private String content;
 }
