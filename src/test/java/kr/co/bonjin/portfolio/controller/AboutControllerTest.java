dcda5e330baaacd0978cba5eb97af7e4682666fa
bonjin-app <bonjin-app@gmail.com>
2021-01-22 18:04:21 +0900

    MockMvc Test ( '/' Mapping) TEST 생성.

--- src/test/java/kr/co/bonjin/portfolio/controller/AboutControllerTest.java
+++ src/test/java/kr/co/bonjin/portfolio/controller/AboutControllerTest.java
@@ -1,9 +1,16 @@
 package kr.co.bonjin.portfolio.controller;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,5 +19,21 @@ class AboutControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
 
+    @BeforeEach
+    void init() {
+        System.out.println("@BeforeEach - executes before each test method in this class");
+    }
+
+    @Test
+    @DisplayName("TEST About View")
+    public void getAboutPage() throws Exception {
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("about"));
+    }
 }
--- src/test/java/kr/co/bonjin/portfolio/controller/HomeControllerTest.java
+++ src/test/java/kr/co/bonjin/portfolio/controller/HomeControllerTest.java
@@ -1,7 +1,40 @@
 package kr.co.bonjin.portfolio.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class HomeControllerTest {
 
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("@BeforeEach - executes before each test method in this class");
+    }
+
+    @Test
+    @DisplayName("TEST Home View")
+    public void getHomePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
 }
