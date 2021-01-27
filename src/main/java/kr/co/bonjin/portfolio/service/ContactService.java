ecb2f94499c2630ee9bbedb0568ada73b3541875
bonjin-app <bonjin-app@gmail.com>
2021-01-27 12:18:30 +0900

    Contact Service 작성.

--- src/main/java/kr/co/bonjin/portfolio/service/ContactService.java
+++ src/main/java/kr/co/bonjin/portfolio/service/ContactService.java
@@ -1,4 +1,13 @@
 package kr.co.bonjin.portfolio.service;
 
+import kr.co.bonjin.portfolio.domain.Contact;
+
 public interface ContactService {
+
+    /**
+     * Contact 저장
+     * @param contact
+     * @return
+     */
+    Long save(Contact contact);
 }
--- src/main/java/kr/co/bonjin/portfolio/service/ContactServiceImpl.java
+++ src/main/java/kr/co/bonjin/portfolio/service/ContactServiceImpl.java
@@ -1,12 +1,22 @@
 package kr.co.bonjin.portfolio.service;
 
+import kr.co.bonjin.portfolio.domain.Contact;
 import kr.co.bonjin.portfolio.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
 
     private final ContactRepository contactRepository;
+
+    @Transactional
+    @Override
+    public Long save(Contact contact) {
+        contactRepository.save(contact);
+        return contact.getId();
+    }
 }
