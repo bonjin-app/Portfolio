d5748d248e02d85433c1b88ae6c51582c8c42461
bonjin-app <bonjin-app@gmail.com>
2021-01-29 12:31:43 +0900

    War 설정 추가.

---
+++ src/main/java/kr/co/bonjin/portfolio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.portfolio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PortfolioApplication.class);
+    }
+
+}
